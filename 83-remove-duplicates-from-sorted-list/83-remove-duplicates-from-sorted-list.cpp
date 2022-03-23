47d81963a32ee77c191bef48d2db72db3595573a
Chan Mee <cmmcme@users.noreply.github.com>
2022-03-24 00:39:39 +0900

    Time: 18 ms (34.55%), Space: 11.6 MB (79.48%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
@@ -12,18 +12,15 @@ class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if(head == NULL || head->next == NULL) return head;
-        ListNode* beforeNode = new ListNode(-200); 
-        ListNode* ret = beforeNode;   
-        // 이전 노드를 강제로 만들어줌. -200은 나올 수 없는 수   
-        
-        while(head != NULL) {
-            if(head->val != beforeNode->val) {
-                beforeNode->next = new ListNode(head->val);
-                beforeNode = beforeNode->next;
+        ListNode* temp = head;           
+        while(temp != NULL && temp->next != NULL) {
+            if(temp->val == temp->next->val) {
+                temp->next = temp->next->next;
+            } else {
+                temp = temp->next;
             }
-            head = head->next;
         }
         
-        return ret->next;
+        return head;
     }
 };
