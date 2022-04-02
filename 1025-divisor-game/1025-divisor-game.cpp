51d832c0e477ba4530060b28e8981c0f087bb74d
Chan Mee <cmmcme@users.noreply.github.com>
2022-04-02 23:26:18 +0900

    Time: 3 ms (39.61%), Space: 6 MB (32.98%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[1010];
+    
+    int func(int num) {
+        if(num == 1) return dp[num] = 0;
+        
+        if(dp[num] != -1) return dp[num];
+        
+        for(int i = 1; i < num; ++i) {
+            if(num % i != 0) continue;
+            
+            if(func(num - i) == 0) return dp[num] = 1;
+        }
+        
+        return dp[num] = 0;
+    }
+    bool divisorGame(int n) {
+        memset(dp, -1, sizeof(dp));
+        func(n);
+        
+        return dp[n];
+    }
+};
