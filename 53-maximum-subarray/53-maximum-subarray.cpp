074657f123b64bec9ec74aaab64856ec881ea65e
Chan Mee <cmmcme@users.noreply.github.com>
2022-04-04 17:18:58 +0900

    Time: 224 ms (6.88%), Space: 67.8 MB (54.65%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {        
     if(nums.size() == 1) return nums[0];
-    
-    vector<int> dp;
-    dp.resize(nums.size() + 1);
-
-    dp[0] = nums[0];
-    int res = dp[0];
-    for(int i = 1; i < nums.size(); ++i) {
-        if(dp[i - 1] + nums[i] > nums[i]) {
-            dp[i] = dp[i - 1] + nums[i];
-        } else {
-            dp[i] = nums[i];
+        
+        int sum = 0;
+        int res = -987654321;
+        for(int i = 0; i < nums.size(); ++i) {
+            if(sum < 0) sum = 0;
+            sum = sum + nums[i];
+            res = max(res, sum);
         }
-        res = max(res, dp[i]);
-    } 
         return res;
     }
 };
