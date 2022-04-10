f0befc135e9e99ec0c90d349f9ea2ec28eed9351
Chan Mee <cmmcme@users.noreply.github.com>
2022-04-11 00:30:54 +0900

    Time: 0 ms (100.00%), Space: 7.7 MB (75.99%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-            int dp[110];
-    memset(dp, -1, sizeof(dp));
-    dp[0] = 0;
-    dp[1] = 0;
-    dp[2] = 0;
-    for(int i = 3; i < nums.size() + 3; ++i) {
-        dp[i] = max(dp[i - 3] + nums[i - 3], dp[i - 2] + nums[i - 3]);
-    }
-    int mmax = 0;
-    for(int i = 0; i < nums.size() + 3; ++i) {
-        mmax = max(mmax, dp[i]);
-    }
+        if(nums.size() == 1) return nums[0];
+        if(nums.size() == 2) return nums[0] > nums[1] ? nums[0] : nums[1];
+        
+        int dp[110];
+        memset(dp, -1, sizeof(dp));
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        int mmax = dp[1];;
+        for(int i = 2; i < nums.size(); ++i) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+            mmax = max(mmax, dp[i]);
+        }
         
         return mmax;
     }
