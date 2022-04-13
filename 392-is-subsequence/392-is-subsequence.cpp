b2a73f60007f2bb6d886d0bfaef7bed7ba72cb60
Chan Mee <cmmcme@users.noreply.github.com>
2022-04-13 23:47:10 +0900

    Time: 76 ms (5.06%), Space: 286.4 MB (5.02%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+int dp[10010][110];
+bool isSubSeq(int tIdx, string t, int sIdx, string s)
+{
+    if(tIdx >= t.size() && sIdx < s.size() - 1) {
+        return false;
+    }
+    
+    if(sIdx == s.size()) {
+        return true;
+    }
+    
+    if(tIdx >= t.size()) return false;
+    
+    if(dp[tIdx][sIdx] != -1) return dp[tIdx][sIdx];
+    
+    if(t[tIdx] == s[sIdx]) {
+        return dp[tIdx][sIdx] = isSubSeq(tIdx + 1, t, sIdx + 1, s);
+    } else {
+        return dp[tIdx][sIdx] =  isSubSeq(tIdx + 1, t, sIdx, s);
+    }
+}
+    bool isSubsequence(string s, string t) {
+        if(s.size() == 0) return true;
+        if(t.size() == 0) return false;
+        
+    memset(dp, -1, sizeof(dp));
+    
+    for(int i = 0; i < t.size(); ++i) {
+        if(t[i] != s[0]) continue;
+        if(isSubSeq(i, t, 0, s)) return true;
+    }
+        
+    
+    return false;
+    }
+};
