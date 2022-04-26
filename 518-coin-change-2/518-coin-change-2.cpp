899d06783fc60a2b07f76777c3abaa434da81335
Chan Mee <cmmcme@users.noreply.github.com>
2022-04-26 18:20:17 +0900

    Time: 3 ms (99.71%), Space: 7 MB (96.39%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int dp[5050] = {-1};
+    int dp[5050] = {0};
     int change(int amount, vector<int>& coins) {
         dp[0] = 1;
         for(int i = 0 ; i < coins.size(); ++i) {
             int coin = coins[i];
             dp[coin] += 1;
-            
             for(int j = coin + 1; j <= amount; ++j) {
-                if(dp[j - coin] != -1) {
+                if(dp[j - coin] != 0) {
                     dp[j] += dp[j-coin];
                 }
             }
