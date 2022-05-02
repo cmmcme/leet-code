246efc3bdab6426ca470d172597b51f1b9d14362
Chan Mee <cmmcme@users.noreply.github.com>
2022-05-02 18:32:02 +0900

    Time: 26 ms (63.86%), Space: 13.3 MB (81.16%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-            vector<vector<int>> graph;
+    vector<vector<int>> graph;
     graph.resize(numCourses + 1);
     vector<int> inNodesEdgeSize;
     inNodesEdgeSize.resize(numCourses + 1);
@@ -17,29 +17,18 @@ public:
     for(int i = 0; i < numCourses; ++i) {
         if(inNodesEdgeSize[i] == 0) que.push(i);
     }
-    vector<bool> visited;
-    visited.resize(numCourses + 1);
-    fill(visited.begin(), visited.begin() + numCourses + 1, false);
-    
+    int cnt = 0;
     while (!que.empty()) {
         int node = que.front();
         que.pop();
-        visited[node] = true;
-        
+        cnt++;
         for(int i = 0; i < graph[node].size(); ++i) {
             int end = graph[node][i];
-            if(visited[end] == false) {
-                inNodesEdgeSize[end]--;
-            }
-            
+            inNodesEdgeSize[end]--;
             if(inNodesEdgeSize[end] == 0) que.push(end);
         }
     }
     
-    for(int i = 0; i < numCourses; ++i) {
-        if(visited[i] == false) return false;
-    }
-    
-    return true;
+    return cnt == numCourses;
     }
 };
