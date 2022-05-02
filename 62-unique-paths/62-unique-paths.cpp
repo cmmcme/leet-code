a2d7f2470be205cd498958a48d9ebe07fd8460ec
Chan Mee <cmmcme@users.noreply.github.com>
2022-05-02 20:10:22 +0900

    Time: 5 ms (14.76%), Space: 5.9 MB (74.75%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int dp[110][110];
-
-int func(int j, int i) {
-    if(j == 0 || i == 0) {
-        dp[j][i] = 1;
-        return 1;
-    }
     
-    if(dp[j][i] != 0) return dp[j][i];
-    
-    return dp[j][i] = func(j - 1, i) + func(j, i - 1);
-}
-    
-    int uniquePaths(int m, int n) {
-            dp[0][0] = 1;
-    
-    func(m-1, n-1);
-    return dp[m-1][n-1];
+    int uniquePaths(int m, int n) {        
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(i==0 || j==0) dp[i][j] = 1;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
