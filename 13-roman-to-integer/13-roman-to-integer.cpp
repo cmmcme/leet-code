5e14e16bbe0689a81757f3e80ad2f5699c3130d1
Chan Mee <cmmcme@users.noreply.github.com>
2022-05-16 22:21:14 +0900

    Time: 9 ms (73.13%), Space: 5.9 MB (86.31%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
+    int getAmount(char ch) {
+        if(ch == 'I') return 1;
+        if(ch == 'V') return 5;
+        if(ch == 'X') return 10;
+        if(ch == 'L') return 50;
+        if(ch == 'C') return 100;
+        if(ch == 'D') return 500;
+        if(ch == 'M') return 1000;
+        
+        return -1;
+    }
     int romanToInt(string s) {
-        map<string, int> mp;
-        mp["I"] = 1;
-        mp["X"] = 10;
-        mp["V"] = 5;
-        mp["L"] = 50;
-        mp["C"] = 100;
-        mp["D"] = 500;
-        mp["M"] = 1000;
-        mp["IV"] = 4;
-        mp["IX"] = 9;
-        mp["XL"] = 40;
-        mp["XC"] = 90;
-        mp["CD"] = 400;
-        mp["CM"] = 900;
-        
-        
         int index = 0;
         int res = 0;
         int before = 0;
         while(index < s.size()) {
-            string sizeOne = s.substr(index, 1);
-            string sizeTwo = s.substr(index, 2);
-            if(mp[sizeOne]) {
-                res += mp[sizeOne];
-                before = mp[sizeOne];
-                index++;
+            int val = getAmount(s[index++]);
+            res += val;
+            if(val > before) {
+                res = res - before - before; 
             }
-                    
-            if(mp[sizeTwo]) {
-                res -= before;
-                res += mp[sizeTwo];
-                index++;
-            }
+            before = val;
         }
         
         return res;
