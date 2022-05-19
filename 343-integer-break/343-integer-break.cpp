7ab5f1c690711d5ab0906c5c2caa791cb647ad91
Chan Mee <cmmcme@users.noreply.github.com>
2022-05-20 00:45:13 +0900

    Time: 6 ms (9.80%), Space: 5.8 MB (98.28%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,23 +1,18 @@
 class Solution {
-public:
+public:    
     int dp[60];
-    
-    int func(int n) {  
-        if(dp[n] != -1) return dp[n];
-        
-        int mmax = 0;
-        for(int i = 1; i < n; ++i) {
-            int currentMax = max(func(n - i), n - i);
-            mmax = max(currentMax * i, mmax);
-        }
-        return dp[n] = mmax;
-    }
-    
     int integerBreak(int n) {
         memset(dp, -1, sizeof(dp));
         dp[0] = 0;
         dp[1] = 0;
         dp[2] = 1;
-        return func(n);
+
+        for(int num = 3; num <= n; ++num) {
+            for(int i = 1; i < num; ++i) {
+                int currentMax = max(dp[num - i], num - i);
+                dp[num] = max(dp[num], currentMax * i);
+            }
+        }
+        return dp[n];
     }
 };
