65686fc9588fc2c1aaf91ed80454090d0d6ce837
Chan Mee <cmmcme@users.noreply.github.com>
2022-05-23 23:57:24 +0900

    Time: 108 ms (11.30%), Space: 31.5 MB (69.28%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int visited[50050];
-    bool bfs (vector<int>& arr, int start) {
+    bool visited[50050];
+    void bfs (vector<int>& arr, int start) {
         queue<int> que;
         que.push(start);
         while(!que.empty()) {
@@ -11,19 +11,17 @@ public:
             if(visited[top]) {
                 continue;
             }
-            
             visited[top] = true;
             
             if(top - arr[top] >= 0) que.push(top - arr[top]);
             if(top + arr[top] < arr.size()) que.push(top + arr[top]);
         }
+    }
+    bool canReach(vector<int>& arr, int start) {
+        bfs(arr, start);
         for(int i = 0 ; i < arr.size(); ++i) {
             if(arr[i] == 0 && visited[i]) return true;
         }
-        
         return false;
-    }
-    bool canReach(vector<int>& arr, int start) {
-        return bfs(arr, start);
     }
 };
