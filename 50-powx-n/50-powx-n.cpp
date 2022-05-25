578806cc613b0c7173f31f9443120a114066cb8e
Chan Mee <cmmcme@users.noreply.github.com>
2022-05-26 02:17:29 +0900

    Time: 0 ms (100.00%), Space: 6 MB (45.61%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        return pow(x,n);
+    double myPow(double x, long long n) {
+        if(n == 0) return 1;
+        if(x == 1) return 1;
+        if(x == 0) return 0;
+        
+        if(n < 0) {
+            n = abs(n);
+            x = 1/x;
+        }
+        
+        double res = 1.0;
+        while(n) {
+            if(n % 2 == 0) {
+                x *= x;
+                n = n / 2;
+            } else {
+                n = n - 1;
+                res *= x;
+            }
+        }
+
+        return res;
     }
 };
