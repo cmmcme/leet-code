718b84c5c5098c2a30c5a3031b52f61943d0bf3f
Chan Mee <cmmcme@users.noreply.github.com>
2022-05-25 20:31:58 +0900

    Time: 8 ms (74.88%), Space: 11 MB (54.04%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,50 +1,42 @@
 class Solution {
 public:
-    
-int dp[550] = {0};
-vector<vector<int>> resultVec;
-bool dyna(vector<int>& candidates, int target) {
-    dp[0] = 1;
-    for(int i = 0; i < candidates.size(); ++i) {
-        int coin = candidates[i];
-        for(int j = coin; j <= target; ++j) {
-            dp[j] += dp[j - coin];
+    vector<vector<int>> resVec;
+    int dp[1010];
+    bool coinFunc(vector<int>& candidates, int target) {
+        dp[0] = 1;
+        for(int i = 0; i < candidates.size(); ++i) {
+            int coin = candidates[i];
+            for(int j = coin; j <= target; ++j) {
+                if(dp[j - coin] == 0) continue;
+                
+                dp[j] += dp[j - coin];
+            }
         }
-    }
-    if(dp[target] == 0) {
-        return false;
-    }
-    return true;
-}
-
-void func(vector<int>& candidates, int target, vector<int>& res, int lastIndex) {
-    if(target == 0) {
-        resultVec.push_back(res);
-        return ;
-    }
-    
-  //  if(resultVec.size() >= 150) return ;
-    
-    for(int i = lastIndex; i < candidates.size(); ++i) {
-        int coin = candidates[i];
-        if(target - coin < 0) continue;
-        if(dp[target - coin] == 0) continue;
         
-        res.push_back(coin);
-        func(candidates, target - coin, res, i);
-        res.pop_back();
+        return dp[target];
     }
-}
-
-vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-    if(!dyna(candidates, target)) return resultVec;
     
-    /*
-     백트래킹 시장
-     */
-    vector<int> vec;
-
-    func(candidates, target, vec, 0);
-    return resultVec;
-}
+    
+    void findCombination(vector<int>& candidates, int target, int index, vector<int>& res) {
+        if(target == 0) {
+            resVec.push_back(res);
+            return ;
+        }
+        
+        for(int i = index; i < candidates.size(); ++i) {
+            if(target - candidates[i] < 0) continue;
+            if(dp[target - candidates[i]] == 0) continue;
+            res.push_back(candidates[i]);
+            findCombination(candidates, target - candidates[i], i, res);
+            res.pop_back();
+        }
+        
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        if(!coinFunc(candidates, target)) return resVec;
+        vector<int> vec;
+        findCombination(candidates, target, 0, vec);
+        
+        return resVec;
+    }
 };
